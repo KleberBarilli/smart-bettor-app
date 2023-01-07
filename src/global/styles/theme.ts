075dc4fb500730f835728ab2032a6f07237eb7f2
kleber.barilli <kleber.barilli73@gmail.com>
2023-01-07 18:53:56 -0300

    feat: themes colors

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+        black: "#000",
+        white: "#fff",
+        primary: "#090715",
+        primaryLight: "#6551e0",
+        secondary: "#008b67",
+        success: "#12a454",
+        danger: "#e83f5b",
+        dark: "#121214",
+        light: "#f1f1f1",
+        gray500: "#a8a8b3",
+        gray800: "#29292e",
+    },
+};
