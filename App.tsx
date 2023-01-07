c024ae10e5ff7dc3d7fcc77ab87a64c94884eace
kleber.barilli <kleber.barilli73@gmail.com>
2023-01-07 19:26:39 -0300

    feat: add google fonts

--- App.tsx
+++ App.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import { Home } from "./src/pages/home";
 import theme from "./src/global/styles/theme";
+import AppLoading from "expo-app-loading";
+import {
+    useFonts,
+    Roboto_400Regular,
+    Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 const App: React.FunctionComponent = () => {
+    const [isFontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+    if (!isFontsLoaded) {
+        return <AppLoading />;
+    }
     return (
         <ThemeProvider theme={theme}>
             <Home />
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -10,5 +10,6 @@ export const Container = styled.View`
 export const Title = styled.Text`
     font-size: 24px;
     font-weight: bold;
+    font-family: ${({ theme }) => theme.fonts.bold};
     color: ${({ theme }) => theme.colors.secondary};
 `;
