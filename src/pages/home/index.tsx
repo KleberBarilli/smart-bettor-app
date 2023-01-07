1e2e94566be0413d8ca248550ee531bf3d0a5a57
kleber.barilli <kleber.barilli73@gmail.com>
2023-01-07 20:02:31 -0300

    feat: header profile

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { Container, Header } from "./styles";
+import {
+    Container,
+    Header,
+    UserInfoDetail,
+    UserAvatar,
+    UserAvatarButton,
+    UserGreeting,
+    UserInfo,
+    UserName,
+    UserWrapper,
+} from "./styles";
+import avatarDefault from "../../assets/avatar1.png";
 
 export const Home: React.FunctionComponent = () => {
     return (
         <Container>
-            <Header></Header>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <UserAvatarButton onPress={() => console.log("sd")}>
+                            <UserAvatar source={avatarDefault} />
+                        </UserAvatarButton>
+                        <UserInfoDetail>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Kleber</UserName>
+                        </UserInfoDetail>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     );
 };
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
-import { RFPercentage } from "react-native-responsive-fontsize";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
     flex: 1;
@@ -9,6 +9,43 @@ export const Container = styled.View`
 export const Header = styled.View`
     width: 100%;
     height: ${RFPercentage(17)}px;
-    font-size: 24px;
     background-color: ${({ theme }) => theme.colors.secondary};
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    padding-top: ${RFValue(28)}px;
+`;
+export const UserWrapper = styled.View`
+    width: 100%;
+    padding: 0 24px;
+`;
+
+export const UserInfo = styled.View`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const UserAvatarButton = styled.TouchableOpacity``;
+
+export const UserAvatar = styled.Image`
+    width: ${RFValue(48)}px;
+    height: ${RFValue(48)}px;
+    border-radius: 10px;
+`;
+
+export const UserInfoDetail = styled.View`
+    margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+    color: ${({ theme }) => theme.colors.gray800};
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+    color: ${({ theme }) => theme.colors.gray800};
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-weight: bold;
 `;
