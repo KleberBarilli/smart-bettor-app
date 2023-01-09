4a1781d794e0414a0f04a0b5126203821a5257ff
kleber.barilli <kleber.barilli73@gmail.com>
2023-01-09 20:19:28 -0300

    signUp use form

--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ScrollView, KeyboardAvoidingView, Platform, View } from "react-native";
+import { useForm, FieldValues } from "react-hook-form";
 import { useNavigation } from "@react-navigation/native";
-import { Input } from "../../components/form/input";
 import { Button } from "../../components/form/button";
 import {
     Content,
@@ -15,13 +15,29 @@ import {
 } from "./styles";
 import logo from "../../assets/logo.png";
 import { Icon } from "../home/styles";
+import { InputControl } from "../../components/form/inputControl";
 
 interface ScreenNavigationProp {
     navigate: (screen: string) => void;
 }
 
+interface IFormInputs {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    [name: string]: any;
+}
+
 export const SignIn: React.FunctionComponent = () => {
+    const { control, handleSubmit } = useForm<FieldValues>();
+
     const { navigate } = useNavigation<ScreenNavigationProp>();
+
+    const handleSignIn = (form: IFormInputs) => {
+        const data = {
+            email: form.email,
+            password: form.password,
+        };
+        console.log(data);
+    };
 
     return (
         <>
@@ -40,9 +56,25 @@ export const SignIn: React.FunctionComponent = () => {
                             <View>
                                 <Title>Faça seu Login</Title>
                             </View>
-                            <Input placeholder="Email" />
-                            <Input placeholder="Senha" />
-                            <Button title="Entrar" />
+                            <InputControl
+                                autoCapitalize="none"
+                                autoCorrect={false}
+                                control={control}
+                                name="email"
+                                placeholder="Email"
+                                keyboardType="email-address"
+                            />
+                            <InputControl
+                                control={control}
+                                name="password"
+                                placeholder="Senha"
+                                autoCorrect={false}
+                                secureTextEntry
+                            />
+                            <Button
+                                title="Entrar"
+                                onPress={() => handleSubmit(handleSignIn)}
+                            />
 
                             <ForgotPasswordButton>
                                 <ForgotPasswordTitle>
