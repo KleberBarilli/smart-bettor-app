bd0f524a5fe58dd60e0ec445539ef86234e45c02
kleber.barilli <kleber.barilli73@gmail.com>
2023-01-28 12:23:26 -0300

    add dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,17 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo','module:metro-react-native-babel-preset'],
+    plugins: [
+        ["module:react-native-dotenv", {
+          "envName": "API_URL",
+          "moduleName": "@env",
+          "path": ".env",
+          "blacklist": null,
+          "whitelist": null,
+          "safe": true,
+          "allowUndefined": true
+        }]
+      ]
   };
 };
